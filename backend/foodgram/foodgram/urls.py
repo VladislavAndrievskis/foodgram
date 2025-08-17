5939f73862ea63ddaea072a1ac6425b33da44ba9
Vladislav Andreev <andrievskisvladislav@gmail.com>
2025-08-17 23:29:45 +0300

    isort

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -2,15 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-
-from rest_framework.routers import DefaultRouter
-
 from ingredients.views import IngredientViewSet
 from recipes.views import RecipeViewSet
+from rest_framework.routers import DefaultRouter
 from tags.views import TagViewSet
 from users.api import UserAvatarView
 from users.views import CustomUserViewSet
-
 
 # Create a router for API endpoints
 router = DefaultRouter()
