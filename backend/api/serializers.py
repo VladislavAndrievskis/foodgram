5df2c93fa3f6ea9db71c752ef5ee41957b19a4ab
Vladislav Andreev <andrievskisvladislav@gmail.com>
2025-08-20 20:00:26 +0300

    serial

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -156,17 +156,15 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     def get_is_favorited(self, obj):
         user = self.context["request"].user
-        return (
-            not user.is_anonymous
-            and user.favorite.filter(recipe=obj).exists()
-        )
+        if user.is_anonymous:
+            return False
+        return user.favorite.filter(recipe=obj).exists()
 
     def get_is_in_shopping_cart(self, obj):
         user = self.context["request"].user
-        return (
-            not user.is_anonymous
-            and user.shopping_cart.filter(recipe=obj).exists()
-        )
+        if user.is_anonymous:
+            return False
+        return user.shopping_cart.filter(recipe=obj).exists()
 
 
 class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
