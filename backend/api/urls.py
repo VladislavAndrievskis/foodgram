dadab860567caceca31707b43cdaa2ad16ba48dc
Vladislav Andreev <andrievskisvladislav@gmail.com>
2025-08-27 21:47:07 +0300

    dj

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -46,6 +46,6 @@ user_urlpatterns = [
 urlpatterns = [
     path("", include(router.urls)),
     path("", include(user_urlpatterns)),
-    path("api/auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.authtoken")),
 ]
