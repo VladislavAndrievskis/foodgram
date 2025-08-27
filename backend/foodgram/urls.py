e5d396078661bc7adec35df83564efce3ec68d59
Vladislav Andreev <andrievskisvladislav@gmail.com>
2025-08-27 22:02:18 +0300

    1

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/auth/", include("djoser.urls")), 
+    path("api/auth/", include("djoser.urls")),
     path("api/", include("api.urls")),
 ]
 
