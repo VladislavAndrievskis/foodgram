235176a29b7765824b6abcfbcf2d9ab1fb64dccf
Vladislav Andreev <andrievskisvladislav@gmail.com>
2025-08-27 21:45:26 +0300

    dj

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -46,5 +46,6 @@ user_urlpatterns = [
 urlpatterns = [
     path("", include(router.urls)),
     path("", include(user_urlpatterns)),
-    path("auth/", include("djoser.urls")),
+    path("api/auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.authtoken")),
 ]
--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -5,7 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-    path("api/auth/", include("djoser.urls.authtoken")),  # только auth token
 ]
 
 if settings.DEBUG:
